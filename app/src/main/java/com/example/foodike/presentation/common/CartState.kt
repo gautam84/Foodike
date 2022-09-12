a3676e6f33a88d4a1f992966d81ae774f227cee2
Gautam <gautamhazarika01@gmail.com>
2022-09-12 14:42:43 +0530

    feat- Added Cart Back-end
    Adding features to complete Foodike food delivery app.

--- app/src/main/java/com/example/foodike/presentation/common/CartState.kt
+++ app/src/main/java/com/example/foodike/presentation/common/CartState.kt
@@ -1,6 +1,5 @@
 package com.example.foodike.presentation.common
 
-import com.example.foodike.domain.model.CartItem
 import com.example.foodike.domain.model.MenuItem
 import com.example.foodike.domain.model.Restaurant
 
@@ -8,3 +7,5 @@ data class CartState(
     val restaurant: Restaurant? = null,
     val list: MutableList<MenuItem> = mutableListOf()
 )
+
+
