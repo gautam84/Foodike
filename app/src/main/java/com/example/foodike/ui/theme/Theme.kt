7430528b669eb253e4ec44d524fa7d6f2134a34f
Gautam <gautamhazarika01@gmail.com>
2023-01-21 23:35:17 +0530

    refactor- Refactoring code for better performance

--- app/src/main/java/com/example/foodike/ui/theme/Theme.kt
+++ app/src/main/java/com/example/foodike/ui/theme/Theme.kt
@@ -1,16 +1,8 @@
 package com.example.foodike.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = LightGreen,
@@ -28,12 +20,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun FoodikeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun FoodikeTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
